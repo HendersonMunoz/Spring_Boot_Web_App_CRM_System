870bafe946bc03c30be9513b027d423afc1a4852
HendersonMunoz <hendersonmunoz52@gmail.com>
2023-07-02 00:38:23 -0400

    3rd commit. Data binding and form input validation

--- src/main/java/com/example/application/views/list/ContactForm.java
+++ src/main/java/com/example/application/views/list/ContactForm.java
@@ -4,6 +4,8 @@ import com.example.application.data.entity.Company;
 import com.example.application.data.entity.Contact;
 import com.example.application.data.entity.Status;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -14,6 +16,8 @@ import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.shared.Registration;
 
 import java.util.List;
 
@@ -60,8 +64,7 @@ public class ContactForm extends FormLayout {
         );
     }
 
-    //API method - that we can use from the view, to set the currently selected contact name.
-    // needed for the data biding and form input validation.
+    //START OF THE API - needed for the data biding and form input validation.
     public void setContact(Contact contact){
         this.contact = contact;
         binder.readBean(contact);
@@ -76,11 +79,76 @@ public class ContactForm extends FormLayout {
         //TERTIARY effect.
         cancel.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
 
+        //THREE CLICK LISTENERS BELOW ARE PART OF THE API CREATED FOR THE CONTACT FORM FUNCTIONALITY.
+        // CLICK LISTENER EVENTS FOR THE THREE BUTTONS WITHIN THE CONTACT FORM - needed for the data biding and form input validation.
+        //adding a click listener for the Save button.
+        save.addClickListener(event -> validateAndSave());
+        //adding a click listener for the Delete button.
+        delete.addClickListener(event -> fireEvent(new DeleteEvent(this, contact)));
+        //adding a click listener for the Close button
+        cancel.addClickListener(event -> fireEvent(new CloseEvent(this)));
+
         //adding click shortcuts
         save.addClickShortcut(Key.ENTER);
         cancel.addClickShortcut(Key.ESCAPE);
 
         // displaying the buttons on a horizontal layout.
         return new HorizontalLayout(save, delete, cancel);
+    }
+
+    // CORRESPONDING METHOD validateAndSave() FOR THE THREE CLICK LISTENERS - PART OF THE API CREATED
+    // FOR THE THREE BUTTONS WITHIN THE CONTACT FORM.
+    private void validateAndSave() {
+        try{
+            binder.writeBean(contact);
+            fireEvent(new SaveEvent(this, contact));
+        } catch (ValidationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // code black below are several Form events - copied from (https://vaadin.com/docs/latest/tutorial/forms-and-validation)
+    // 'Setup component events' section of the tutorial.
+    public static abstract class ContactFormEvent extends ComponentEvent<ContactForm> {
+        private Contact contact;
+
+        protected ContactFormEvent(ContactForm source, Contact contact) {
+            super(source, false);
+            this.contact = contact;
+        }
+
+        public Contact getContact() {
+            return contact;
+        }
+    }
+
+    public static class SaveEvent extends ContactFormEvent {
+        SaveEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+    }
+
+    public static class DeleteEvent extends ContactFormEvent {
+        DeleteEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+
+    }
+
+    public static class CloseEvent extends ContactFormEvent {
+        CloseEvent(ContactForm source) {
+            super(source, null);
+        }
+    }
+
+    public Registration addDeleteListener(ComponentEventListener<DeleteEvent> listener) {
+        return addListener(DeleteEvent.class, listener);
+    }
+
+    public Registration addSaveListener(ComponentEventListener<SaveEvent> listener) {
+        return addListener(SaveEvent.class, listener);
+    }
+    public Registration addCloseListener(ComponentEventListener<CloseEvent> listener) {
+        return addListener(CloseEvent.class, listener);
     }
 }
