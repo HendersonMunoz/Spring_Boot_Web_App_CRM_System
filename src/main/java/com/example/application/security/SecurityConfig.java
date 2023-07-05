98d04d76e2699f72d33cff16b34e9d212875cdf8
HendersonMunoz <hendersonmunoz52@gmail.com>
2023-07-04 23:00:45 -0400

    th commit. debugging the Login/Logout functionality. SecurityConfig class, making it so that the '/line-awesome/**' file path is ignored when logging in.

--- src/main/java/com/example/application/security/SecurityConfig.java
+++ src/main/java/com/example/application/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,10 +18,14 @@ public class SecurityConfig extends VaadinWebSecurity {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers("/images/*.png").permitAll();
         super.configure(http);
         setLoginView(http, LoginView.class);
+    }
+
+    @Override
+    protected void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers("/line-awesome/**");
+        super.configure(web);
     }
 
     @Bean
